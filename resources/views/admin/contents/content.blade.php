48ecca7d168533b983e3909889d4dd1c33486df4
Davi Esteves <desenvolvedores@codejr.com.br>
2021-12-22 00:08:22 -0300

    icones para doc e excel

--- resources/views/admin/contents/content.blade.php
+++ resources/views/admin/contents/content.blade.php
@@ -3,16 +3,43 @@
         <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0 align-top" style="max-height:410px; max-width:410px">
             <div class="card-body m-0 p-0">
                 <div class="text-center w-100 m-0 p-0">
-                    <img class="img-fluid m-0 p-0" style="max-height:400px; max-width:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!">
+                    <img load="lazy" class="img-fluid m-0 p-0" style="max-height:400px; max-width:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!">
                 </div>
             </div>
         </div>
     @endif
-    @if(strstr($content->type,'video/') || strstr($content->type,'gif/'))
-        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0" style="max-width:800px; max-height:400px">
+    @if(strstr($content->type,'video/'))
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0 align-top" style="max-width:810px; max-height:410px">
             <div class="card-body m-0 p-0">
                 <div class="text-center w-100 m-0 p-0">
-                    <iframe class="img-fluid m-0 p-0" style="width:800px; height:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!"></iframe>
+                    <video controls pause="true" load="lazy" class="img-fluid m-0 p-0" style="width:800px; height:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!"></video>
+                </div>
+            </div>
+        </div>
+    @endif
+    @if(strstr($content->type,'pdf'))
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0" style="max-width:800px; max-height:800px">
+            <div class="card-body m-0 p-0">
+                <div class="text-center w-100 m-0 p-0">
+                    <iframe load="lazy" class="img-fluid m-0 p-0" style="min-width:400px; min-height:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!"></iframe>
+                </div>
+            </div>
+        </div>
+    @endif
+    @if(strstr($content->type,'text/rtf') || strstr($content->type,'application/msword') || strstr($content->type,'application/vnd.openxmlformats-officedocument.wordprocessingml.document'))
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0 align-top" style="max-height:410px; max-width:410px">
+            <div class="card-body m-0 p-0">
+                <div class="text-center w-100 m-0 p-0">
+                    <img load="lazy" class="img-fluid m-0 p-0" style="max-height:400px; max-width:400px" src="{{ asset('img/doc-file.png') }}" alt="documento não encontrada!">
+                </div>
+            </div>
+        </div>
+    @endif
+    @if(strstr($content->type,'text/csv') || strstr($content->type,'application/vnd.ms-excel') || strstr($content->type,'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'))
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0 align-top" style="max-height:410px; max-width:410px">
+            <div class="card-body m-0 p-0">
+                <div class="text-center w-100 m-0 p-0">
+                    <img load="lazy" class="img-fluid m-0 p-0" style="max-height:400px; max-width:400px" src="{{ asset('img/excel-file.png') }}" alt="documento não encontrada!">
                 </div>
             </div>
         </div>
