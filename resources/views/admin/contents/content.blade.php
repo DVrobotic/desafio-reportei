33b71b2601e2cc1298421bc72078dbb9a3c4b574
Davi Esteves <desenvolvedores@codejr.com.br>
2021-12-21 23:11:40 -0300

    responsividade

--- resources/views/admin/contents/content.blade.php
+++ resources/views/admin/contents/content.blade.php
@@ -9,7 +9,7 @@
         </div>
     @endif
     @if(strstr($content->type,'video/') || strstr($content->type,'gif/'))
-        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-auto my-3 p-0" style="max-width:800px; max-height:400px">
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0" style="max-width:800px; max-height:400px">
             <div class="card-body m-0 p-0">
                 <div class="text-center w-100 m-0 p-0">
                     <iframe class="img-fluid m-0 p-0" style="width:800px; height:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!"></iframe>
