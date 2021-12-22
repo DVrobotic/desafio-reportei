ddfdacb241f867d2a75097f32a43d26faa27acad
Davi Esteves <desenvolvedores@codejr.com.br>
2021-12-21 23:10:32 -0300

    consertando responsividade

--- app/Http/Requests/BoxRequest.php
+++ app/Http/Requests/BoxRequest.php
@@ -26,7 +26,7 @@ class BoxRequest extends FormRequest
         return [
             'name' => 'required|string|min:3|max:255|unique:boxes' . ($this->getMethod() != "PUT" ?  '':',name,' . $this->box->id),
             'content_list' => "array|min:1|max:20",
-            'content_list.*' => 'file|mimes:jpeg,png,bmp,svg,webp,doc,docx,pdf,csv,xls,xlsx,gif,ico,mp3,mp4,mov,ogg,qt,weba,webp,mpeg|max:20000',
+            'content_list.*' => 'file|mimes:jpeg,png,bmp,svg,webp,doc,docx,pdf,csv,xls,xlsx,gif,ico,mp3,mp4,mov,ogg,qt,weba,webp,mpeg|max:50000',
         ];
     }
 }
--- resources/views/admin/contents/content.blade.php
+++ resources/views/admin/contents/content.blade.php
@@ -1,9 +1,18 @@
 @foreach($contents as $content)
     @if(strstr($content->type,'image/'))
-        <div class="card card-dark card-outline  border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0" style="max-height:410px; max-width:410px">
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-3 my-3 p-0 align-top" style="max-height:410px; max-width:410px">
             <div class="card-body m-0 p-0">
                 <div class="text-center w-100 m-0 p-0">
                     <img class="img-fluid m-0 p-0" style="max-height:400px; max-width:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!">
+                </div>
+            </div>
+        </div>
+    @endif
+    @if(strstr($content->type,'video/') || strstr($content->type,'gif/'))
+        <div class="card card-dark card-outline border-dark col-md-5 col-12 d-inline-block mx-auto my-3 p-0" style="max-width:800px; max-height:400px">
+            <div class="card-body m-0 p-0">
+                <div class="text-center w-100 m-0 p-0">
+                    <iframe class="img-fluid m-0 p-0" style="width:800px; height:400px" src="{{ asset($content->file_path) }}" alt="Imagem não encontrada!">
                 </div>
             </div>
         </div>
