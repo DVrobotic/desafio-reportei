651c72980ab56d47ff4a60c49d999fb71bb96e78
Davi Esteves <desenvolvedores@codejr.com.br>
2021-12-24 06:12:33 -0300

    config de app para hospedagem

--- config/app.php
+++ config/app.php
@@ -28,6 +28,8 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
+    'key' => env('APP_KEY', 'base64:iAzVXQxhVOhOoF2xUH/m2qY2i8qepGbOUqf6XUPHrDc='),
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
