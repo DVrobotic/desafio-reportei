fb5f4eba4de3f19cb399f9b875806453a76dd40f
Davi Esteves <desenvolvedores@codejr.com.br>
2021-12-23 21:11:25 -0300

    adicionado titulo a video e pdf

--- resources/views/admin/contents/pdfContent.blade.php
+++ resources/views/admin/contents/pdfContent.blade.php
@@ -1,7 +1,10 @@
 <div class="mb-5 deletable pdf-card card card-dark card-outline border-left-0 border-right-0 border-bottom-0 col-md-5 col-12 d-inline-block mx-3 my-3 p-0" style="max-width:800px; max-height:800px">
+    <div style="max-width:800.2px" class="mx-auto card-header border-right border-left border-bottom-0">
+        {{ $content->name }}
+    </div>
     <div class="card-body m-0 p-0">
         <div class="text-center w-100 m-0 p-0">
-            <iframe load="lazy" class="img-fluid m-0 p-0" style="min-width:400px; min-height:400px" src="{{ asset($content->file_path) }}" alt="Pdf não encontrada!"></iframe>
+            <iframe frameborder="0" load="lazy" class="img-fluid m-0 p-0" style="min-width:400px; min-height:400px" src="{{ asset($content->file_path) }}" alt="Pdf não encontrada!"></iframe>
         </div>
     </div>
     <div class="card-footer p-1">
